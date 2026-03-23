5d31bd5dcd04ace137f2915edfb56431806d7f9e
Miguel Herize <migherize@gmail.com>
2026-03-22 21:06:38 -0300

    feat(api): add configuration capacitor to android and ios mobile

---
+++ android/app/src/main/java/com/tuempresa/bookmyspot/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tuempresa.bookmyspot;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tuempresa.bookmyspot',
+  appName: 'app_citas',
+  webDir: 'dist'
+};
+
+export default config;
